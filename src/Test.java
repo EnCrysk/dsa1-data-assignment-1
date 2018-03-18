55a55c5843b70c843d56fe1b2cd647e7927932f5
EnCrysk <duymark42@gmail.com>
2018-03-18 23:39:50 +0700

    Sua loi cau a

--- src/Test.java
+++ src/Test.java
@@ -7,6 +7,8 @@ public class Test {
     public static void main(String[] args)
     {
         System.out.println("Xong cau a");
+        System.out.println("Xong cau a");
+        System.out.println("Xong cau a");
     }
     
 }
